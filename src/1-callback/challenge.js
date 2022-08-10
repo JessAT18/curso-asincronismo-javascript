84ffc59827e7339c9f1d2d7822eb5f5286c0e414
JessAT18 <j-aquino@outlook.com>
2022-08-10 16:36:11 -0400

    XMLHTTPRequest hasta el 12:59

---
+++ src/1-callback/challenge.js
@@ -0,0 +1,16 @@
+const XMLHttpRequest = require('xmlhttprequest');
+
+const API = 'https://api.escuelajs.co/api/v1/';
+
+function fetchData(urlApi, callback) {
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET', urlApi, true);
+    xhttp.onreadystatechange = function(event) {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 200) {
+                callback(null, JSON.parse(xhttp.responseText));
+            }
+        }
+    };
+}
